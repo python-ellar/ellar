9c3df6502e344891b3c8d8a04dff5a954dcfbc70
eadwinCode <ezeudoh.tochukwu@gmail.com>
2022-03-04 21:35:07 +0100

    export definitions

--- compatible/__init__.py
+++ compatible/__init__.py
@@ -1,3 +1,12 @@
 from .cache_properties import cached_property, locked_cached_property
 from .emails import EmailStr
 from .dict import AttributeDictAccess, DataMapper
+
+
+__all__ = [
+    'cached_property',
+    'locked_cached_property',
+    'EmailStr',
+    'AttributeDictAccess',
+    'DataMapper',
+]
