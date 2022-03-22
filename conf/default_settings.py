8c98acd353609fe92c593d8d9339673d13e3aa69
eadwinCode <ezeudoh.tochukwu@gmail.com>
2022-03-22 15:13:12 +0100

    moved default_lifespan_handler to settings

--- conf/default_settings.py
+++ conf/default_settings.py
@@ -2,11 +2,15 @@ import typing as t
 
 from starlette.middleware import Middleware
 from starlette.responses import JSONResponse
-
-from starletteapi.exceptions import RequestValidationError, APIException
+from starlette.websockets import WebSocketClose
+from starletteapi.types import TScope, TReceive, TSend
+from starletteapi.responses import PlainTextResponse
+from starletteapi.exceptions import RequestValidationError, APIException, StarletteHTTPException
 from starletteapi.versioning import DefaultAPIVersioning, BaseAPIVersioning
 from starletteapi.exception_handlers import api_exception_handler, request_validation_exception_handler
 
+if t.TYPE_CHECKING:
+    from starletteapi.main import StarletteApp
 
 DEBUG: bool = False
 
@@ -33,3 +37,24 @@ APP_EXCEPTION_HANDLERS: t.Dict[t.Union[int, t.Type[Exception]], t.Callable] = {
 # Exception handler callables should be of the form
 # `handler(request, exc) -> response` and may be be either standard functions, or async functions.
 USER_CUSTOM_EXCEPTION_HANDLERS: t.Dict[t.Union[int, t.Type[Exception]], t.Callable] = {}
+
+
+async def _not_found(scope: TScope, receive: TReceive, send: TSend) -> None:
+    if scope["type"] == "websocket":
+        websocket_close = WebSocketClose()
+        await websocket_close(scope, receive, send)
+        return
+
+    # If we're running inside a starlette application then raise an
+    # exception, so that the configurable exception handler can deal with
+    # returning the response. For plain ASGI apps, just return the response.
+    if "app" in scope:
+        raise StarletteHTTPException(status_code=404)
+    else:
+        response = PlainTextResponse("Not Found", status_code=404)
+    await response(scope, receive, send)
+
+DEFAULT_NOT_FOUND_HANDLER: t.Callable[[TScope, TReceive, TSend], t.Coroutine[t.Any, t.Any, None]] = _not_found
+# The lifespan context function is a newer style that replaces
+# on_startup / on_shutdown handlers. Use one or the other, not both.
+DEFAULT_LIFESPAN_HANDLER: t.Optional[t.Callable[["StarletteApp"], t.AsyncContextManager]] = None
