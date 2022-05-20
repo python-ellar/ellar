c0565c34713e88c7d33e75e8d47ada864e1d86f7
eadwinCode <ezeudoh.tochukwu@gmail.com>
2022-05-20 17:44:53 +0100

    Typing update

--- ellar/common/decorators/modules.py
+++ ellar/common/decorators/modules.py
@@ -1,23 +1,25 @@
 import typing as t
 from pathlib import Path
 
+from starlette.routing import Mount
+
 from ellar.core.guard import GuardCanActivate
 from ellar.core.modules import (
     ApplicationModuleDecorator,
     BaseModuleDecorator,
     ModuleDecorator,
 )
-from ellar.core.routing import ControllerDecorator, ModuleRouter, OperationDefinitions
+from ellar.core.routing import ControllerDecorator, ModuleRouter
 from ellar.di import ProviderConfig
 
 
 def Module(
     *,
     name: t.Optional[str] = None,
-    controllers: t.Sequence[ControllerDecorator] = tuple(),
-    routers: t.Sequence[t.Union[ModuleRouter, OperationDefinitions]] = tuple(),
+    controllers: t.Sequence[t.Union[ControllerDecorator, t.Type]] = tuple(),
+    routers: t.Sequence[t.Union[ModuleRouter, Mount]] = tuple(),
     providers: t.Sequence[t.Union[t.Type, ProviderConfig]] = tuple(),
-    template_folder: t.Optional[str] = None,
+    template_folder: t.Optional[str] = "templates",
     base_directory: t.Optional[t.Union[Path, str]] = None,
     static_folder: str = "static"
 ) -> ModuleDecorator:
@@ -38,10 +40,10 @@ def ApplicationModule(
         t.Union[t.Type, BaseModuleDecorator, ModuleDecorator]
     ] = tuple(),
     global_guards: t.List[t.Union[t.Type[GuardCanActivate], GuardCanActivate]] = None,
-    controllers: t.Sequence[ControllerDecorator] = tuple(),
-    routers: t.Sequence[t.Union[ModuleRouter, OperationDefinitions]] = tuple(),
+    controllers: t.Sequence[t.Union[ControllerDecorator, t.Type]] = tuple(),
+    routers: t.Sequence[t.Union[ModuleRouter, Mount]] = tuple(),
     providers: t.Sequence[t.Union[t.Type, ProviderConfig]] = tuple(),
-    template_folder: t.Optional[str] = None,
+    template_folder: t.Optional[str] = "templates",
     base_directory: t.Optional[t.Union[Path, str]] = None,
     static_folder: str = "static"
 ) -> ApplicationModuleDecorator:
