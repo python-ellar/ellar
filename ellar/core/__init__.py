a7a4d2fd098ba1b5693744c77746309b9e19cf74
eadwinCode <ezeudoh.tochukwu@gmail.com>
2022-05-30 10:31:43 +0100

    Refactored ControllerBase importa

--- ellar/core/__init__.py
+++ ellar/core/__init__.py
@@ -1,10 +1,10 @@
 from .conf import Config
 from .context import ExecutionContext
+from .controller import ControllerBase
 from .factory import AppFactory
 from .guard import BaseAPIKey, BaseAuthGuard, BaseHttpAuth, GuardCanActivate
 from .main import App
 from .modules import ModuleBase
-from .routing import ControllerBase
 from .templating import render_template, render_template_string
 from .testclient import TestClient, TestClientFactory
 
