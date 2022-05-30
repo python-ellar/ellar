994cc22b2543e13fe9c9070b0cbd02c2bd3d469e
eadwinCode <ezeudoh.tochukwu@gmail.com>
2022-05-30 09:53:14 +0100

    Refactored set_meta to use reflect service

--- ellar/common/decorators/base.py
+++ ellar/common/decorators/base.py
@@ -2,38 +2,19 @@ import typing as t
 from functools import partial
 
 from ellar.constants import NOT_SET
-from ellar.core.operation_meta import OperationMeta
+from ellar.reflect import reflect
 
 
-def _operation_wrapper(
-    meta_key: str, meta_value: t.Any, target: t.Union[t.Callable, t.Any]
-) -> t.Union[t.Callable, t.Any]:
-    _meta = getattr(target, "_meta", OperationMeta())
-    _meta_values: t.Any = [meta_value]
-    existing = _meta.get(meta_key)
-    if existing:
-        if isinstance(existing, (list, tuple)):
-            _meta_values.extend(existing)
-            if isinstance(existing, tuple):
-                _meta_values = tuple(_meta_values)
-        elif isinstance(existing, set):
-            _meta_values.extend(existing)
-            _meta_values = set(_meta_values)
-        elif isinstance(existing, dict):
-            _meta_values = dict(meta_value)
-            _meta_values.update(existing)
-    else:
-        _meta_values = meta_value
-    _meta.update({meta_key: _meta_values})
-    setattr(target, "_meta", _meta)
-    return target
-
-
-def set_meta(meta_key: t.Any, meta_value: t.Optional[t.Any] = NOT_SET) -> t.Callable:
+def set_meta(
+    meta_key: t.Any,
+    meta_value: t.Optional[t.Any] = NOT_SET,
+    default_value: t.Any = None,
+) -> t.Callable:
     if meta_value is NOT_SET:
         return partial(set_meta, meta_key)
 
     def _decorator(target: t.Union[t.Callable, t.Any]) -> t.Union[t.Callable, t.Any]:
-        return _operation_wrapper(meta_key, meta_value, target)
+        reflect.define_metadata(meta_key, meta_value, target, default_value)
+        return target
 
     return _decorator
