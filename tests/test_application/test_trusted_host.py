e77d77081d392fdf899a6afd48caa509ac644700
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2022-12-23 18:13:35 +0100

    Added test for cors and trustedhost middleware

---
+++ tests/test_application/test_trusted_host.py
@@ -0,0 +1,53 @@
+from starlette.requests import Request
+from starlette.responses import PlainTextResponse
+
+from ellar.common import get
+from ellar.core import TestClientFactory
+
+
+def test_trusted_host_middleware(test_client_factory):
+    @get()
+    def homepage(request: Request):
+        return PlainTextResponse("OK", status_code=200)
+
+    tm = TestClientFactory.create_test_module()
+    tm.app.router.append(homepage)
+
+    tm.app.config.ALLOWED_HOSTS = ["testserver", "*.testserver"]
+
+    tm.app.rebuild_middleware_stack()
+    client = tm.get_client()
+
+    response = client.get("/")
+    assert response.status_code == 200
+
+    client = tm.get_client(base_url="http://subdomain.testserver")
+    response = client.get("/")
+    assert response.status_code == 200
+
+    client = tm.get_client(base_url="http://invalidhost")
+    response = client.get("/")
+    assert response.status_code == 400
+
+
+def test_default_allowed_hosts():
+    tm = TestClientFactory.create_test_module()
+    assert tm.app.config.ALLOWED_HOSTS == ["*"]
+
+
+def test_www_redirect(test_client_factory):
+    @get()
+    def homepage(request: Request):
+        return PlainTextResponse("OK", status_code=200)
+
+    tm = TestClientFactory.create_test_module()
+    tm.app.router.append(homepage)
+
+    tm.app.config.ALLOWED_HOSTS = ["www.example.com"]
+
+    tm.app.rebuild_middleware_stack()
+
+    client = tm.get_client(base_url="https://example.com")
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.url == "https://www.example.com/"
---
+++ tests/test_routing/test_route_endpoint_params.py
@@ -0,0 +1,91 @@
+from starlette.requests import (
+    HTTPConnection as StarletteHTTPConnection,
+    Request as StarletteRequest,
+)
+from starlette.responses import Response as StarletteResponse
+from starlette.websockets import WebSocket as StarletteWebSocket
+
+from ellar.common import Ctx, Http, Req, Res, Ws
+from ellar.core import ExecutionContext, TestClientFactory
+from ellar.core.connection import (
+    HTTPConnection as EllarHTTPConnection,
+    Request as EllarRequest,
+    WebSocket as EllarWebSocket,
+)
+from ellar.core.context import IExecutionContext
+from ellar.core.routing import ModuleRouter
+
+router = ModuleRouter()
+
+
+@router.get("/starlette-request")
+def get_requests(request: StarletteRequest, req=Req()):
+    assert isinstance(request, EllarRequest)  # True
+    assert isinstance(req, EllarRequest)
+    return req == request
+
+
+@router.get("/ellar-context")
+def get_requests(
+    context_1: IExecutionContext, context_2: ExecutionContext, context_3=Ctx()
+):
+    assert isinstance(context_2, ExecutionContext)  # True
+    assert isinstance(context_1, ExecutionContext)
+    return context_1 == context_2 == context_3
+
+
+@router.get("/starlette-connection")
+def get_connections(connection: StarletteHTTPConnection, connection_2=Http()):
+    assert isinstance(connection, EllarHTTPConnection)  # True
+    assert isinstance(connection_2, EllarHTTPConnection)
+    return connection == connection_2
+
+
+@router.get("/starlette-response")
+def get_responses(response: StarletteResponse, response_2=Res()):
+    assert isinstance(response, StarletteResponse)  # True
+    assert isinstance(response_2, StarletteResponse)
+    return response == response_2
+
+
+@router.ws_route("/starlette-websocket")
+async def get_websockets(websocket: StarletteWebSocket, ws=Ws()):
+    assert isinstance(ws, EllarWebSocket)  # True
+    assert isinstance(websocket, EllarWebSocket)
+    await ws.accept()
+    await ws.send_json(ws == websocket)
+    await ws.close()
+
+
+tm = TestClientFactory.create_test_module(routers=[router])
+client = tm.get_client()
+
+
+def test_get_connections():
+    response = client.get("/starlette-connection")
+    assert response.status_code == 200
+    assert response.json() is True
+
+
+def test_get_request():
+    response = client.get("/starlette-request")
+    assert response.status_code == 200
+    assert response.json() is True
+
+
+def test_get_context():
+    response = client.get("/ellar-context")
+    assert response.status_code == 200
+    assert response.json() is True
+
+
+def test_get_response():
+    response = client.get("/starlette-response")
+    assert response.status_code == 200
+    assert response.json() is True
+
+
+def test_get_websocket():
+    with client.websocket_connect("/starlette-websocket") as session:
+        data = session.receive_json()
+        assert data is True
