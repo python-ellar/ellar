fc7df7a9368d6f59fc56765be9a7ad68bbf2a5d4
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2023-03-03 07:03:40 +0100

    added a custom client for PyMemcache library

--- ellar/cache/backends/pymem_cache.py
+++ ellar/cache/backends/pymem_cache.py
@@ -13,10 +13,42 @@ except ImportError as e:  # pragma: no cover
 from .base import BasePylibMemcachedCache
 
 
+class Client(HashClient):
+    """pymemcache client.
+    Customize pymemcache behavior as python-memcached (default backend)'s one.
+    """
+
+    def __init__(self, servers: t.List[str], *args: t.Any, **kwargs: t.Any) -> None:
+
+        super(Client, self).__init__(
+            self._split_host_and_port(servers), *args, **kwargs
+        )
+
+    def _split_host_and_port(self, servers: t.List[t.Any]) -> t.List[t.Tuple[str, int]]:
+        """Convert python-memcached based server strings to pymemcache's one.
+        - python-memcached: ['127.0.0.1:11211', ...] or ['127.0.0.1', ...]
+        - pymemcache: [('127.0.0.1', 11211), ...]
+        """
+        _host_and_port_list = []
+        for server in servers:
+            connection_info = server.split(":")
+            if len(connection_info) == 1:
+                _host_and_port_list.append((connection_info[0], 11211))
+            elif len(connection_info) == 2:
+                _host_and_port_list.append(
+                    (connection_info[0], int(connection_info[1]))
+                )
+        return _host_and_port_list
+
+    def disconnect_all(self) -> None:
+        for client in self.clients.values():
+            client.close()
+
+
 class PyMemcacheCacheBackend(BasePylibMemcachedCache):
     """An implementation of a cache binding using pymemcache."""
 
-    MEMCACHE_CLIENT = HashClient
+    MEMCACHE_CLIENT = Client
 
     def __init__(self, servers: t.List[str], options: t.Dict = None, **kwargs: t.Any):
 
