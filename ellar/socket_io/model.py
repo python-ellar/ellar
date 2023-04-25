9ab3b40bfbe4cfa5a2e7e5e3cd5be951e29b2fac
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2023-04-25 15:40:52 +0100

    refactored GatewayContext

--- ellar/socket_io/model.py
+++ ellar/socket_io/model.py
@@ -1,21 +1,8 @@
-import typing as t
-
-from engineio import AsyncServer
-
-if t.TYPE_CHECKING:
-    from ellar.core import IExecutionContext
+from .context import GatewayContext
 
 
 class GatewayType(type):
     pass
-
-
-class GatewayContext(t.NamedTuple):
-    server: AsyncServer
-    sid: str
-    message: t.Any
-    context: "IExecutionContext"
-    environment: t.Dict
 
 
 class GatewayBase(metaclass=GatewayType):
