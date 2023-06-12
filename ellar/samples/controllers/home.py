a34742705f3f4cb1876a9f035f4743d9fc86388d
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2023-06-12 02:52:18 +0100

    fixed some documentation missing images

--- ellar/samples/controllers/home.py
+++ ellar/samples/controllers/home.py
@@ -1,3 +1,4 @@
+import ellar
 from ellar.common import Controller, ControllerBase, get, render
 
 
@@ -6,8 +7,13 @@ class HomeController(ControllerBase):
     @get("/")
     @render()
     def index(self):
+        github_url = "https://github.com/eadwincode/ellar"
+        doc_url = "https://eadwincode.github.io/ellar/"
         return {
             "message": "",
-            "docs_url": "https://eadwincode.github.io/ellar/",
-            "git_hub": "https://github.com/eadwincode/ellar",
+            "docs_url": doc_url,
+            "git_hub": github_url,
+            "version": ellar.__version__,
+            "release_url": f"{github_url}/releases/{ellar.__version__}",
+            "example_project_url": f"{github_url}/tree/main/examples",
         }
