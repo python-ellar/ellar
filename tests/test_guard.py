8286c1f9bdf75d9d9449c30676bca526ec390bb5
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2023-06-19 07:41:44 +0100

    Added test for allow_any_guard decorator

--- tests/test_guard.py
+++ tests/test_guard.py
@@ -1,7 +1,14 @@
 import pytest
 from starlette.status import HTTP_401_UNAUTHORIZED
 
-from ellar.common import APIException, Req, UseGuards, get, serialize_object
+from ellar.common import (
+    APIException,
+    Req,
+    UseGuards,
+    allow_any_guard,
+    get,
+    serialize_object,
+)
 from ellar.core import AppFactory, Reflector
 from ellar.core.guards import (
     APIKeyCookie,
@@ -257,3 +264,21 @@ def test_global_guard_works():
     data = res.json()
 
     assert data == {"detail": "Forbidden"}
+
+
+def test_allow_any_guard():
+    _app = AppFactory.create_app(global_guards=[DigestAuth])
+
+    @get("/global")
+    @allow_any_guard
+    def _auth_demo_endpoint():
+        return {"message": "ok"}
+
+    _app.router.append(_auth_demo_endpoint)
+    _client = TestClient(_app)
+    res = _client.get("/global")
+
+    assert res.status_code == 200
+    data = res.json()
+
+    assert data == {"message": "ok"}
