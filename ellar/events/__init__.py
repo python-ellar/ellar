6abe28c61fbf22ad120e8d4b1798b59574e3c6bc
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2023-12-30 12:56:29 +0100

    removed request context events

--- ellar/events/__init__.py
+++ ellar/events/__init__.py
@@ -1,14 +1,9 @@
 from .base import EventHandler, EventManager
 
-request_context_started_events = EventManager()
-request_context_teardown_events = EventManager()
-
 app_context_started_events = EventManager()
 app_context_teardown_events = EventManager()
 
 __all__ = [
-    "request_context_started_events",
-    "request_context_teardown_events",
     "app_context_started_events",
     "app_context_teardown_events",
     "EventHandler",
