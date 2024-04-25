e921d06ef70f02c530f80ce713981bda45f9e03a
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2024-04-25 09:08:31 +0100

    fail_silently log to debug

--- ellar/di/utils.py
+++ ellar/di/utils.py
@@ -1,10 +1,15 @@
+import logging
 import typing as t
+
+logger = logging.getLogger("ellar")
 
 
 @t.no_type_check
 def fail_silently(func: t.Callable, *args: t.Any, **kwargs: t.Any) -> t.Optional[t.Any]:
     try:
         return func(*args, **kwargs)
-    except Exception as ee:  # pragma: no cover
-        print(ee)
+    except Exception as ex:  # pragma: no cover
+        logger.debug(
+            f"Calling {func} with args: {args} kw: {kwargs} failed\nException: {ex}"
+        )
     return None
