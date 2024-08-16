8cfbb4be70fdad5b7ec8807abb2d355cd809f190
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2024-08-16 07:18:27 +0100

    Fixing all samples

--- samples/01-carapp/carapp/config.py
+++ samples/01-carapp/carapp/config.py
@@ -1,9 +1,21 @@
 from ellar.core.conf import ConfigDefaultTypesMixin
 
 
-class DevelopmentConfig(ConfigDefaultTypesMixin):
+class BaseConfig(ConfigDefaultTypesMixin):
+    MIDDLEWARE = [
+        "ellar.core.middleware.trusted_host:trusted_host_middleware",
+        "ellar.core.middleware.cors:cors_middleware",
+        "ellar.core.middleware.errors:server_error_middleware",
+        "ellar.core.middleware.versioning:versioning_middleware",
+        "ellar.auth.middleware.session:session_middleware",
+        "ellar.auth.middleware.auth:identity_middleware",
+        "ellar.core.middleware.exceptions:exception_middleware",
+    ]
+
+
+class DevelopmentConfig(BaseConfig):
     DEBUG = True
 
 
-class TestingConfig(ConfigDefaultTypesMixin):
+class TestingConfig(BaseConfig):
     DEBUG = False
